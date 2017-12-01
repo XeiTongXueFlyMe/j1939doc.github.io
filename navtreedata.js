e14ee20292bd9ce345f1de1e37bfd1c51da8ad46
xieming <2067057918@qq.com>
2017-12-01 11:35:39 +0800

    【增加】增加请求与响应的API说明

--- navtreedata.js
+++ navtreedata.js
@@ -2,22 +2,24 @@ var NAVTREE =
 [
   [ "J1939Socket", "index.html", [
     [ "历史版本变更", "index.html", [
-      [ "J1939Socket API Version 2", "index.html#autotoc_md17", null ],
-      [ "J1939Socket API Version 1", "index.html#autotoc_md18", null ]
+      [ "J1939Socket API Version 2", "index.html#autotoc_md21", null ],
+      [ "J1939Socket API Version 1", "index.html#autotoc_md22", null ]
     ] ],
     [ "J1939Socket API", "labelid1.html", [
-      [ "接口函数概要表", "labelid1.html#autotoc_md0", null ],
+      [ "API 函数概要表", "labelid1.html#autotoc_md0", null ],
       [ "J1939_Initialization()", "labelid1.html#autotoc_md2", null ],
       [ "J1939_Poll()", "labelid1.html#autotoc_md4", null ],
       [ "J1939_EnqueueMessage()", "labelid1.html#autotoc_md6", null ],
       [ "J1939_DequeueMessage()", "labelid1.html#autotoc_md8", null ],
       [ "J1939_TP_TX_Message()", "labelid1.html#autotoc_md10", null ],
-      [ "J1939_TP_RX_Message()", "labelid1.html#autotoc_md12", null ]
+      [ "J1939_TP_RX_Message()", "labelid1.html#autotoc_md12", null ],
+      [ "J1939_Request_PGN()", "labelid1.html#autotoc_md14", null ],
+      [ "J1939_Create_Response()", "labelid1.html#autotoc_md16", null ]
     ] ],
     [ "J1939协议栈框架", "labelid2.html", [
-      [ "文件组成概述", "labelid2.html#autotoc_md14", null ],
-      [ "功能拓扑图", "labelid2.html#autotoc_md15", null ],
-      [ "函数组成结构", "labelid2.html#autotoc_md16", null ]
+      [ "文件组成概述", "labelid2.html#autotoc_md18", null ],
+      [ "功能拓扑图", "labelid2.html#autotoc_md19", null ],
+      [ "函数组成结构", "labelid2.html#autotoc_md20", null ]
     ] ],
     [ "结构体", "annotated.html", [
       [ "结构体", "annotated.html", "annotated_dup" ],
